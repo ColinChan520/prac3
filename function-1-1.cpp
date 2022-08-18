bf70ddce08f0a3c37cc8285d26ee8d42387bd5fd
ColinChan520 <104440913+ColinChan520@users.noreply.github.com>
2022-08-18 19:02:29 +0930

    1

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -2,8 +2,8 @@
 #include <iomanip>
 using namespace std;
 
-void copy_2d_strings(string first[][2], string second[][2], int rows) {
-for(int i=0; i<rows; i++)
+void string_2d_copy(string first[][2], string second[][2], int n) {
+for(int i=0; i<n; i++)
 for(int j=0; j<2; j++)
 second[i][j] = first[i][j];
 }
--- main-1-1.cpp
+++ main-1-1.cpp
@@ -9,7 +9,7 @@ string first[3][2] = {{"New York","Los Angeles"},
 {"Chicago","Houston"},
 {"Philadelphia","San Diego"}};
 string second[3][2];
-copy_2d_strings(first, second, 3);
+string_2d_copy(first, second, 3);
 cout<<"Values of second two-dimensional array after copy:"<<endl<<endl;
 for(int i=0; i<3; i++) {
 for(int j=0; j<2; j++) {
